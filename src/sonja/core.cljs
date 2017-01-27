f14b83e050288e22e0ac42653b946d40842d31cf
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2017-01-27 20:06:30 +0100

    Make the noise less diagonal.

--- src/sonja/core.cljs
+++ src/sonja/core.cljs
@@ -36,7 +36,7 @@
      (.drawImage context image 0 0)
      (let [image-data (get-image-data context)
            pixels (.-data image-data)]
-       (dorun (map #(aset ^ints pixels (+ % (rand-int 4)) (+ 50 (rand-int 50))) (range 0 wh 12)))
+       (dorun (map #(aset ^ints pixels (+ % (rand-int 12)) (+ 50 (rand-int 50))) (range 0 wh 12)))
        (put-image-data context image-data))
        nil)))
 
